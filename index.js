bdc459326f281fe48d58912cb014db29c7435f81
Murad H Sarker <sarker24@gmail.com>
2022-05-23 23:54:21 +0200

    Connect with mongodb and create connection

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const cli = require("nodemon/lib/cli");
 
-const port = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nzwqa.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  await client.connect();
+  const itemConnection = client.db("electrix").collection("items");
+
+  // get data
+
+  app.get("/item", async (req, res) => {
+    const query = {};
+    const cursor = itemConnection.find(query);
+    const items = await cursor.toArray();
+    res.send(items);
+  });
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello Manufacturer Application");
+  res.send("Hello ElectriX");
 });
 
 app.listen(port, () => {
-  console.log(`Manufacturer application run on port ${port}`);
+  console.log(`Electrix application run on port ${port}`);
 });
