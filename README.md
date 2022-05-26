059dadfbba4bcefe40737fc31854be7a6968e7cc
Murad H Sarker <sarker24@gmail.com>
2022-05-26 22:43:48 +0200

    Heroku push first phase

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 ## Manufacturar application
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://electrix_admin:<password>@cluster0.nzwqa.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-const collection = client.db("test").collection("devices");
-// perform actions on the collection object
-client.close();
-});
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cli = require("nodemon/lib/cli");
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -20,15 +20,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
   await client.connect();
-  const itemConnection = client.db("electrix").collection("items");
+  const itemCollection = client.db("electrix").collection("items");
 
   // get data
 
   app.get("/item", async (req, res) => {
     const query = {};
-    const cursor = itemConnection.find(query);
+    const cursor = itemCollection.find(query);
     const items = await cursor.toArray();
     res.send(items);
+  });
+
+  app.get("/item/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const query = { _id: ObjectId(id) };
+    const item = await itemCollection.findOne(query);
+    res.send(item);
+  });
+
+  app.post("/item", async (req, res) => {
+    const newItem = req.body;
+    const result = await itemCollection.insertOne(newItem);
+    res.send(result);
   });
 }
 run().catch(console.dir);
